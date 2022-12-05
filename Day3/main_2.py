251caa26ca136d86f604e858782937c8d4352e76
Russell Johnson <russ@johnsonville.net>
2022-12-04 22:43:21 -0600

    Day 3 completd

--- Day3/main_2.py
+++ Day3/main_2.py
@@ -1,7 +1,7 @@
 priority_sum = 0
 alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
-with open("Day3/input_test.txt", 'r') as f:
+with open("Day3/input.txt", 'r') as f:
     badge_priority_sum = 0
     elf_1 = []
     elf_2 = []
@@ -11,22 +11,24 @@ with open("Day3/input_test.txt", 'r') as f:
     for x in f:
         full_sack.append(x.strip())
     max_rows = len(full_sack)
-    for row_num in range(max_rows):
+    max_iter = int(max_rows/3)
+    print(len(full_sack))
+    for row_num in range(max_iter):
         print(f"{row_num} {current}")
-        for x in range(3):
-            print(current)
-            elf_1 = full_sack[current]
+        if current < max_rows:
+            elf_1 = set(full_sack[current])
             current += 1
             elf_2 = full_sack[current]
             current += 1
             elf_3 = full_sack[current]
             current += 1
-        print(elf_1)
-        print(elf_2)
-        print(elf_3)
-        for item in elf_1:
-            if item in elf_2 and item in elf_3:
-                badge_priority_sum += alpha.index(item) + 1
+            print(elf_1)
+            print(elf_2)
+            print(elf_3)
+            for item in elf_1:
+                if item in elf_2 and item in elf_3:
+                    print(item)
+                    badge_priority_sum += alpha.index(item) + 1
     print(badge_priority_sum)
 
 
