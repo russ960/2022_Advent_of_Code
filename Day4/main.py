e40cfdd1f65df4b04987e7a992aec37601e7bada
Russell Johnson <russ@johnsonville.net>
2022-12-07 21:04:26 -0600

    day 4 part 1 fixed

--- Day4/main.py
+++ Day4/main.py
@@ -1,12 +1,15 @@
 count = 0
 with open("Day4/input.txt", 'r') as f:
-    for section in f:
-        section_clean = section.strip()
-        primary, secondary = section_clean.split(',')
-        primary = primary.split('-')
-        secondary = secondary.split('-')
-        if secondary[0] >= primary[0] and secondary[1] <= primary[1]:
-            count += 1
-        # elif primary[0] >= secondary[0] and primary[1] <= secondary[1]:
-        #     count += 1
-    print(count)
+    section_clean = [value for value in f.read().strip().split("\n")]
+        
+for vals in section_clean:
+    primary, secondary = vals.split(',')
+    primary = [int(value) for value in primary.split('-')]
+    secondary = [int(value) for value in secondary.split('-')]
+    if primary[0] <= secondary[0] and primary[1] >= secondary[1]:
+        count += 1
+        print(f"{primary[0]}-{primary[1]}  {secondary[0]}-{secondary[1]}")
+    elif secondary[0] <= primary[0] and secondary[1] >= primary[1]:
+        count += 1
+        print(f"{primary[0]}-{primary[1]}  {secondary[0]}-{secondary[1]}")
+print(count)
